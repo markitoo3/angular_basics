ef220674b9b4924b98e3d39fa3220c30887d6805
markitoo3 <dietrich.marko@gmail.com>
2022-08-03 15:26:58 +0200

    made some tasks

--- typescript-01-basics/dist/basics.js
+++ typescript-01-basics/dist/basics.js
@@ -1,68 +1,41 @@
+"use strict";
 /* ELSŐ feladat */
 /*  Hozz létre egy Video classt title, uploader és seconds fieldekkel.
     Legyen egy watch metódusa is, ami kiírja a film címét és a perceket.
     Hozz létre két példányt a classból és hívd meg mindkettőnek a watch metódusát. */
-
-
-
 class Video {
-    private _title: string;
-    private _uploader: string;
-    private _seconds: number;
-
-    constructor(title: string, uploader: string, seconds: number) {
+    constructor(title, uploader, seconds) {
         this._title = title;
         this._uploader = uploader;
         this._seconds = seconds;
     }
-
-    public watch(): string {
-        return `title: ${this._title}, length: ${Math.floor(this._seconds / 60)} min`
+    watch() {
+        return `title: ${this._title}, length: ${Math.floor(this._seconds / 60)} min`;
     }
 }
-
 const movie1 = new Video('Lord of the Rings', 'Mark', 7200);
 const movie2 = new Video('Jurassic Park', 'James Cameron', 4000);
 console.log(movie1.watch());
 console.log(movie2.watch());
-
- 
-    
-    
-    
 /* MÁSODIK feladat */
 /*  Hozz létre egy Person classt, name és age fieldekkel.
     Legyen egy describe metódusa, ami visszaadja a fieldek értékét.
     Hozz létre két példányt, majd hívd meg mindkettőn a describe metódust.*/
-
 class Person {
-    //SAME IN ENGLISH
 }
-
-
-
 /* HARMADIK feladat*/
 /*  Hozz létre egy User interface-t, name, age és position (feltételes) fieldekkel.
-    Hozz létre egy tömböt ami két user konkrét user objektumot tartalmaz, 
+    Hozz létre egy tömböt ami két user konkrét user objektumot tartalmaz,
     majd egy függvény segítségével írd ki a userek nevét és a korukat.
     
     Hozz létre egy Role enumot, Admin és User értékekkel, majd definiáld az interface-ben, és implementáld a classban!
     */
-
-enum Role {
-    Admin,
-    User
-}
-
-interface User {
-    name: string;
-    age: number;
-    position?: string;
-    role?: Role;
-}
-
-
-const users: Array<User> = [
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["User"] = 1] = "User";
+})(Role || (Role = {}));
+const users = [
     {
         name: 'Béla',
         age: 45,
@@ -74,10 +47,8 @@ const users: Array<User> = [
         role: Role.User,
         position: 'developer'
     }
-]
-
-const printUserNameAndAge = (userArr: User[]) => {
+];
+const printUserNameAndAge = (userArr) => {
     userArr.forEach(user => console.log(user.name, user.age));
-}
-
+};
 printUserNameAndAge(users);
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -4,3 +4,26 @@ const newPerson: Person = new Person('Tibi', 40);
 
 console.log(newPerson.getName());
 console.log(newPerson.getAge());
+
+
+
+class User {
+    public username: string;
+    public static ip_adress: string = '192.168.1.1';
+    private _email: string;  //"email?:" string vagy undefined VAGY  "string | undefined"
+    private _password: string;
+
+
+
+    constructor(username: string, email: string, password: string) {
+        this.username = username;
+        this._email = email;
+        this._password = password;
+    }
+}
+
+const user1 = new User('Lacika', 'a@b.c', 'asdasd');
+
+// user1.username csak ezt éred el, a többi private
+
+// User.ip_adress csak a Classon keresztül érhető el, példányból nem (static)
